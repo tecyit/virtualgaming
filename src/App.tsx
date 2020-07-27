3d284814e009e5a775514c28a041686c51f03170
Dalin Oluoch <mcdalinoluoch@gmail.com>
2020-07-27 13:15:48 +0300

    Add semantic ui react add node sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import * as styles from './test.module.scss';
 
 function App() {
-  return <h2>bitmis</h2>;
+  return (
+    <>
+      <h2 className={styles.title}>bitmis</h2>
+      <Button>Click</Button>
+    </>
+  );
 }
 
 export default App;
