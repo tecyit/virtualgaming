edcc91492d246f3bab5534948f42f0cbe87b4e00
Dalin Oluoch <mcdalinoluoch@gmail.com>
2020-09-14 02:44:05 +0300

    Remove helmet

--- client/src/Pages/Dashboard.tsx
+++ client/src/Pages/Dashboard.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import MainLayout from '../Components/Layouts/MainLayout';
 import TabMenu from '../Components/Nav/TabMenu';
 
 const Dashboard = () => {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Home</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
       <MainLayout>
         <TabMenu />
       </MainLayout>
